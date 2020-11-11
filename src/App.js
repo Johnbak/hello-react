0baf6d7043b609756d4dbc2a79f627f2007099be
LAPTOP-D7K1N972\worst <worstgrim@gmail.com>
2020-11-11 14:54:34 +0700

    component render condition , state same getter setter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  const count = 0;
+  let var3 = 0;
   const var1 = "Hello";
   const var2 = true;
 
+  //same : let count = 0 / Getter Setter
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Hi BRO : {count}</h1>
+      <h1>Hi BRO : {var3}</h1>
       <h1>Hi BRO : {var1}</h1>
       <h1>Hi BRO : {var2 && "YES"}</h1>
-      {var2 ? <h1>Yess</h1>:<h1>No</h1>}
+      {var2 ? <h1>Yess</h1> : <h1>No</h1>}
+
+      <h1>StatCount : {count}</h1>
+
+      <button
+        onClick={() => {
+          var3 = 10;
+          setCount(count+1)
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 }
