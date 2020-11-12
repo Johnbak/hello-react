eec121148e1696b8639b9d3f7e2fe31991a73c0b
LAPTOP-D7K1N972\worst <worstgrim@gmail.com>
2020-11-12 16:47:37 +0700

    @Test : Props Type and Default prop

--- src/components/ex3_props.js
+++ src/components/ex3_props.js
@@ -1,10 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export default function ex3_props(props) {
   return (
     <div>
       {props.isShowLabel && <h1> Count Label</h1>}
+      {props.defaultPropLabel && <h1> Default Label</h1>}
       <h1 style={{ color: props.color }}>{props.count}</h1>
     </div>
   );
 }
+
+ex3_props.propTypes = {
+  count: PropTypes.number,
+};
+
+ex3_props.defaultProps = {
+  defaultPropLabel: true,
+};
